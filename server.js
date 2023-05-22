c5f678e859635300e0a1f143572d8c5cd8a56e9b
Francisco Rabiella <paccote@hotmail.com>
2023-05-21 21:24:24 -0600

    ajustes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/app_pelis_udg'));
+app.use(express.static('./dist/appmovie'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app_pelis_udg/'}),
+    res.sendFile('index.html', {root: 'dist/appmovie/'}),
 );
 
 // Start the app by listening on the default Heroku port
